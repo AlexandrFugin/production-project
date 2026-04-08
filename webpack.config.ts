d1b263742f205c0696c68632eb6c70d719bf04a1
a.fugin <a.fugin@zaryad.tech>
2026-04-08 21:17:57 +0300

    lesson 13

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import htmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnv, BuildPaths} from "./config/build/types/config";
@@ -8,7 +7,8 @@ export default (env: BuildEnv) => {
   const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html')
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src'),
   }
 
   const mode = env.mode || 'development';
