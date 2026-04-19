43f4c3ea2380e130a7988e3060e8107439d1ad3a
a.fugin <a.fugin@zaryad.tech>
2026-04-19 22:34:34 +0300

    lesson 25

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,69 +5,59 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import i18next from 'eslint-plugin-i18next';
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        __IS_DEV__: true,
-      },
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
+export default defineConfig([{
+  files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  plugins: { js },
+  extends: ["js/recommended"],
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      __IS_DEV__: true,
     },
   },
-
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    rules: {
-      "no-unused-vars": "warn",
-      "react/jsx-indent": ["error", 2],
-      "react/jsx-indent-props": ["error", 2],
-      indent: ["error", 2],
-      "react/jsx-filename-extension": 
-        ["error", { extensions: [".js", ".jsx", ".tsx"] }],
-      "import/no-unresolved": "off",
-      "import/prefer-default-export": "off",
-      "react/require-default-props": "off",
-      "react/jsx-props-no-spreading": "warn",
-      "react/function-component-definition": "off",
-      "no-shadow": "off",
-      "import/extensions": "off",
-      "import/no-extraneous-dependencies": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-      "no-underscore-dangle": "off",
-      "i18next/no-literal-string": [
-        'error',
-        {
-          markupOnly: true,
-          ignoreAttribute: ['data-testid', 'to']
-        }
-      ],
-      "max-len": ['error', { "ignoreComments": true, code: 100 }]
+  settings: {
+    react: {
+      version: "detect",
     },
   },
-  {
-    files: ["**/*.{ts,tsx,mts,cts}"],
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/ban-ts-comment": "off",
-    },
+}, tseslint.configs.recommended, pluginReact.configs.flat.recommended, {
+  files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react/jsx-indent": ["error", 2],
+    "react/jsx-indent-props": ["error", 2],
+    indent: ["error", 2],
+    "react/jsx-filename-extension": 
+      ["error", { extensions: [".js", ".jsx", ".tsx"] }],
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
+    "react/require-default-props": "off",
+    "react/jsx-props-no-spreading": "warn",
+    "react/function-component-definition": "off",
+    "no-shadow": "off",
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "no-underscore-dangle": "off",
+    "i18next/no-literal-string": [
+      'error',
+      {
+        markupOnly: true,
+        ignoreAttribute: ['data-testid', 'to']
+      }
+    ],
+    "max-len": ['error', { "ignoreComments": true, code: 100 }]
   },
-  i18next.configs['flat/recommended'],
-  {
-    files: ['**/src/**/*.test.ts', '**/src/**/*.test.tsx'],
-    rules: {
-      'i18next/no-literal-string': 'off',
-    },
+}, {
+  files: ["**/*.{ts,tsx,mts,cts}"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/ban-ts-comment": "off",
   },
-]);
+}, i18next.configs['flat/recommended'], {
+  files: ['**/src/**/*.test.ts', '**/src/**/*.test.tsx'],
+  rules: {
+    'i18next/no-literal-string': 'off',
+  },
+}]);
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import {FC, ButtonHTMLAttributes} from "react";
 
 export enum ThemeButton {
   CLEAR = 'clear',
+  OUTLINE = 'outline',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
