835b14e2200c9164e5ffedc5d57868cc6872dc05
a.fugin <a.fugin@zaryad.tech>
2026-04-19 13:06:27 +0300

    lesson 24

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 import {useTranslation} from "react-i18next";
+import {Button} from "../../../../shared/ui/Button/Button";
 
 interface SidebarProps {
   className?: string;
@@ -19,15 +20,14 @@ export const Sidebar = ({className}: SidebarProps) => {
 
   return (
     <div
-      className={classNames(
-        cls.Sidebar,
-        {[cls.collapsed]: collapsed},
-        [className]
-      )}
-    >
-      <button onClick={onToggle}>
+      data-testid="sidebar"
+      className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+      <Button
+        data-testid='sidebar-toggle'
+        onClick={onToggle}
+      >
         {t('toggle')}
-      </button>
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
