b6f914829634312e33636ed6763c60c6a7a32b13
a.fugin <a.fugin@zaryad.tech>
2026-04-26 22:59:55 +0300

    lesson 30

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -56,6 +56,7 @@ export default defineConfig([{
     "max-len": ['error', { "ignoreComments": true, code: 100 }],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
+    "no-param-reassign": "off",
   },
 }, {
   files: ["**/*.{ts,tsx,mts,cts}"],
