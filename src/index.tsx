9bf1ece6a4b2ba8716bab7cb6077d509a66d3afa
a.fugin <a.fugin@zaryad.tech>
2026-04-27 22:16:41 +0300

    lesson 31

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom";
-import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "app/providers/ThemeProvider";
+import {StoreProvider} from "app/providers/StoreProvider";
 
+import App from "./app/App";
 import 'app/styles/index.scss';
 import "./shared/config/i18n/i18n";
 import {ErrorBoundary} from "app/providers/ErrorBoundary";
-import {StoreProvider} from "app/providers/StoreProvider";
+
 
 ReactDOM.render(
   <StoreProvider>
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,8 +1,8 @@
 import {classNames} from "shared/lib/classNames/classNames";
-import cls from './Modal.module.scss'
 import React, {ReactNode, useCallback, useEffect, useRef, useState} from "react";
 import {Portal} from "shared/ui/Portal/Portal";
 import {useTheme} from "app/providers/ThemeProvider";
+import cls from './Modal.module.scss'
 
 interface ModalProps {
   className?: string;
@@ -59,7 +59,6 @@ export const Modal = (props: ModalProps) => {
   const mods: Record<string, boolean> = {
     [cls.opened]: isOpen,
     [cls.isClosing]: isClosing,
-    [cls[theme]]: true,
   }
 
   return (
