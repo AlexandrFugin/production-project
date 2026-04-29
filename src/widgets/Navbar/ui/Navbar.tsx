0c1c1f584c53c3af17edc41b847a8a05d02b8deb
a.fugin <a.fugin@zaryad.tech>
2026-04-29 08:58:28 +0300

    lesson 32

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,8 +2,8 @@ import React, {useCallback, useState} from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
 import {useTranslation} from "react-i18next";
-import {Modal} from "shared/ui/Modal/Modal";
 import {Button, ButtonTheme} from "shared/ui/Button/Button";
+import {LoginModal} from "features/AuthByUsername";
 
 interface NavbarProps {
   className?: string;
@@ -13,8 +13,12 @@ export const Navbar = ({className}: NavbarProps) => {
   const {t} = useTranslation();
   const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false);
+  },[])
+
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true);
   },[])
 
   return (
@@ -22,13 +26,14 @@ export const Navbar = ({className}: NavbarProps) => {
       <Button
         className={cls.links}
         theme={ButtonTheme.CLEAR_INVERTED}
-        onClick={onToggleModal}
+        onClick={onShowModal}
       >
         {t('Войти')}
       </Button>
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        {t('Текст в модальном окне')}
-      </Modal>
+      <LoginModal
+        isOpen={isAuthModal}
+        onClose={onCloseModal}
+      />
     </div>
   );
 };
