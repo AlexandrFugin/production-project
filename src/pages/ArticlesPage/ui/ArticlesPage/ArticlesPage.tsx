9cbe3efdb882eba19266d19dac7d66d83eb0d5d0
a.fugin <a.fugin@zaryad.tech>
2026-05-28 22:24:52 +0300

    lesson 48

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,20 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTranslation} from "react-i18next";
+import cls from './ArticlesPage.module.scss'
+import {memo} from "react";
+
+interface ArticlesPageProps {
+  className?: string;
+}
+
+const ArticlesPage = ({className}: ArticlesPageProps) => {
+  const {t} = useTranslation('article');
+
+  return (
+    <div className={classNames(cls.ArticlesPage, {}, [className])}>
+      {t('ARTICLES PAGE')}
+    </div>
+  );
+};
+
+export default memo(ArticlesPage);
