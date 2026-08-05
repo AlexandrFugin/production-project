ef087910318361a5167d1420179268cb54d0a445
a.fugin <a.fugin@zaryad.tech>
2026-08-05 21:12:02 +0300

    lesson 104

---
+++ scripts/clear-cache.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
+const cachePath = path.resolve(__dirname, '..', 'node_modules', '.cache');
+
+fs.rmSync(cachePath, { recursive: true, force: true });
