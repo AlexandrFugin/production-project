87befae8675f3f81321fd4157d7eecd4f6a5a50b
a.fugin <a.fugin@zaryad.tech>
2026-09-07 00:21:43 +0300

    lesson 139

--- src/shared/ui/redesigned/AppLogo/AppLogo.tsx
+++ src/shared/ui/redesigned/AppLogo/AppLogo.tsx
@@ -19,14 +19,14 @@ export const AppLogo = memo((props: AppLogoProps) => {
       justify={'center'}
       className={classNames(cls.appLogoWrapper, {}, [className])}
     >
-      <div className={cls.gradientBig}></div>
-      <div className={cls.gradientSmall}></div>
       <AppSvg
         width={size}
         height={size}
         color="black"
         className={cls.appLogo}
       ></AppSvg>
+      <div className={cls.gradientBig}></div>
+      <div className={cls.gradientSmall}></div>
     </HStack>
   );
 });
