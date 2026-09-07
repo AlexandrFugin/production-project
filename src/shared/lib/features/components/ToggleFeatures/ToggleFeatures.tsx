22734f6971fc094c8e4b4a9c1bac7d4d8aa08d89
a.fugin <a.fugin@zaryad.tech>
2026-09-07 23:39:54 +0300

    lesson 142

--- src/shared/lib/features/components/ToggleFeatures/ToggleFeatures.tsx
+++ src/shared/lib/features/components/ToggleFeatures/ToggleFeatures.tsx
@@ -1,6 +1,6 @@
 import { FeatureFlags } from '@/shared/types/featureFlags';
 import { ReactElement } from 'react';
-import { getFeatureFlag } from '../getSetFeatures';
+import { getFeatureFlag } from '../../lib/getSetFeatures';
 
 interface ToggleFeaturesProps {
   feature: keyof FeatureFlags;
---
+++ src/shared/lib/features/services/updateFeatureFlags.ts
@@ -0,0 +1,34 @@
+import { FeatureFlags } from '@/shared/types/featureFlags';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ThunkConfig } from '@/app/providers/StoreProvider';
+import { updateFeatureFlagsMutation } from '../api/featureFlagsApi';
+import { getAllFeatureFlags } from '../lib/getSetFeatures';
+
+interface UpdateFeatureFlagsOptions {
+  userId: string;
+  newFeatures: Partial<FeatureFlags>;
+}
+
+export const updateFeatureFlag = createAsyncThunk<
+  void,
+  UpdateFeatureFlagsOptions,
+  ThunkConfig<string>
+>('user/saveJsonSettings', async ({ userId, newFeatures }, thunkApi) => {
+  const { rejectWithValue, dispatch } = thunkApi;
+
+  try {
+    await dispatch(
+      updateFeatureFlagsMutation({
+        userId,
+        features: {
+          ...getAllFeatureFlags(),
+          ...newFeatures,
+        },
+      }),
+    );
+    window.location.reload();
+  } catch (e) {
+    console.log(e);
+    return rejectWithValue('');
+  }
+});
